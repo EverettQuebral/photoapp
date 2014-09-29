bf1b48b6430cd328105244f3fcba8e70dba6294d
equebral <equebral@paypal.com>
2014-09-29 13:55:23 -0700

    initial site

--- public/app.js
+++ public/app.js
@@ -33,21 +33,9 @@ if ('development' == app.get('env')) {
 
 urls(app);
 
+//connection.getPhotos(1,10);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// var express = require('express')
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000))
-// app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// })
-
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
