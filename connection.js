3fccb390a922457205b0204823fa99669072d585
equebral <equebral@paypal.com>
2014-10-02 11:10:53 -0700

    simplifying external requests

--- connection.js
+++ connection.js
@@ -15,7 +15,6 @@ var Connection = function(){
 			}
 		};
 
-
 	return {
 		getPhotos : function(req, res){
 			var method = '&method=flickr.photosets.getPhotos',
@@ -31,57 +30,24 @@ var Connection = function(){
 			//options.path = options.path + '&method=flickr.people.getPublicPhotos&nojsoncallback=1' + page + perPage;
 
 
-			var request,
+			var request = require('request'),
 				jsonOut = "";
 
 
 			console.log("OPTIONS" ,options);
 
+			request('https://' + options.hostname + options.path, function(error, response, body){
+				if (error) res.json(error);
 
-			request = https.request(options, function(result){
-				result.setEncoding('utf8');
-				result.on('data', function(d){
-					jsonOut += d;
-				});
-				result.on('end', function(){
-					if (jsonOut.length > 0){
-						try {
-							var data = JSON.parse(jsonOut);
-							var photos = data.photoset.photo;
-							var paging = {
-								page : data.photoset.page,
-								perpage : data.photoset.per_page,
-								total : data.photoset.total
-							};
-							var pages = [];
-							var i = 1;
-							for (i = 0; i <= (paging.total/100); ++i){
-								pages.push(i + 1);
-							}
+				var data = JSON.parse(body);
+				var pages = [];
+				var i = 1;
+				for (i = 0; i <= (data.photoset.total / 100); ++i){
+					pages.push(i + 1);
+				}
 
-
-							console.log("pages", pages);
-							console.log("paging", paging);
-							//console.log(data);
-							//console.log(photos);
-							res.render('photos', {photolist : photos, pages : pages, host: req.get('host'), photosetId : req.param('photosetid'), currentpage : req.param('page')});
-						}
-						catch (e){
-							console.log(e);
-							res.send('Sorry, the server encountered an error.  Please try again later ' + e);
-						}
-						
-					}
-					else {
-						res.json({
-							error : 'Error encountered',
-							description : 'error eh'
-						});
-					}
-				});
-			}).on('error', function(e){
-				console.log(e);
-			}).end();
+				res.render('photos', {photolist : data.photoset.photo, pages : pages, host: req.get('host'), photosetId : req.param('photosetid'), currentpage : req.param('page')});
+			});
 		}
 	}
 }();
