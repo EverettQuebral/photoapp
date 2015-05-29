20202a6a03105b9808a6fbbaa1a18137c1684c15
equebral <equebral@paypal.com>
2015-05-29 10:19:02 -0700

    adding newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Everett Photo App'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '51415ae518f0551a207a91bcc3045afe3bc65ce9',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
